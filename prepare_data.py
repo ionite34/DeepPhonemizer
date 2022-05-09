ab5f9f8fb3093a2947663241c58f1f7933fb4806
Ionite <ionite@pm.me>
2022-05-09 13:06:33 -0400

    Updated data prepare methods

--- prepare_data.py
+++ prepare_data.py
@@ -11,9 +11,21 @@ _origin_file = './dicts/cmu.txt'
 def main():
     # First read the origin file
     origin_dict = DictReader(_origin_file).dict
+    origin_cur_len = len(origin_dict)
+    origin_additions = 0
     # Scan all files in the directory
     for file in tqdm(sorted(os.listdir(_target_dir))):
         if file.endswith('.txt') and file != 'cmu.txt':
+            # Read the file
+            target = DictReader(os.path.join(_target_dir, file)).dict
+            # Add the new words to the origin dictionary
+            origin_dict.update(target)
+            # Count the additions
+            diff = len(origin_dict) - origin_cur_len
+            if diff > 0:
+                origin_additions += diff
+                origin_cur_len = len(origin_dict)
+
 
 
 
