7840bf69e0cdb35db93d22a1b71ab235b8abb717
Ionite <dev@ionite.io>
2022-05-10 14:35:26 -0400

    Implemented data prepare features

--- prepare_data.py
+++ prepare_data.py
@@ -1,12 +1,13 @@
 import json
 import os
 
+import numpy as np
 from tqdm import tqdm
 from dict_reader import DictReader
 from format_ph import *
 
-_target_dir = './dicts/'
-_origin_file = './dicts/cmu.dict'
+_target_dir = './dicts_prod/'
+_origin_file = './dicts_prod/cmu_core.dict'
 
 
 # Convert json dictionary to double space delimited dictionary
@@ -25,6 +26,7 @@ def convert_json_to_dict(origin_file, target_dir):
         for word in data_dict.keys():
             f.write(f'{word}  {to_sds(data_dict[word])}\n')
 
+
 # Main
 def main():
     # First read the origin file
@@ -37,7 +39,7 @@ def main():
     added_words = set()
     # Scan all files in the directory
     for file in tqdm(sorted(os.listdir(_target_dir)), position=0):
-        if file.endswith('.dict') and file != 'cmu.dict':
+        if file.endswith('_delta.dict') and not file.endswith('_core.dict'):
             # Read the file
             target = DictReader(os.path.join(_target_dir, file)).dict
             # Record the additions
@@ -59,11 +61,31 @@ def main():
     print(f'Total additions: {len(added_words)}')
     print(f'Total words: {len(origin_dict)}')
 
+    # Loop through the final dictionary and remove any words that contain a '(' and ')'
+    for word in list(updated_dict.keys()):
+        if '(' in word and ')' in word:
+            updated_dict.pop(word)
+
+    # Sort the dictionary into a list of word keys by word length
+    sorted_dict = sorted(updated_dict.keys(), key=lambda x: len(x))
+    # Randomly sample 10% evenly distributed words for validation
+    sample_size = int(len(sorted_dict) * 0.1)
+    data_validation = np.random.choice(sorted_dict, sample_size, replace=False)
+    # Use the rest for training
+    data_training = list(set(sorted_dict) - set(data_validation))
+    # Write to files
+    with open(os.path.join(_target_dir, 'data_training.dict'), 'w') as f:
+        for word in tqdm(data_training, desc='Writing training data'):
+            f.write(f'{word}  {to_sds(updated_dict[word])}\n')
+    with open(os.path.join(_target_dir, 'data_validation.dict'), 'w') as f:
+        for word in tqdm(data_validation, desc='Writing validation data'):
+            f.write(f'{word}  {to_sds(updated_dict[word])}\n')
+
 
 if __name__ == '__main__':
-    # main()
-    origin_f = './dicts/rb-f4.json'
-    target_d = './dicts/'
-    convert_json_to_dict(origin_f, target_d)
+    main()
+    # origin_f = './dicts/rb-f4.json'
+    # target_d = './dicts/'
+    # convert_json_to_dict(origin_f, target_d)
     print('-' * 5)
     print('Finished')
